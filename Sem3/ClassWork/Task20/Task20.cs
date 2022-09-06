e5d1f74037e4cccdb1cb1e7f53eab65325858e9b
Malikov Vladimir <v.a.jack@rambler.ru>
2022-09-06 21:41:29 +0300

    Fix

--- Sem3/ClassWork/Task20/Task20.cs
+++ Sem3/ClassWork/Task20/Task20.cs
@@ -1,13 +1,18 @@
 ﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
 
-Console.WriteLine("Введите координату X1: ");
-int x1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите координату Y1: ");
-int y1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите координату X2: ");
-int x2 = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите координату Y2: ");
-int y2 = int.Parse(Console.ReadLine());
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите координату X1: ");
+        int x1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите координату Y1: ");
+        int y1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите координату X2: ");
+        int x2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите координату Y2: ");
+        int y2 = int.Parse(Console.ReadLine());
 
-double result = Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2));
-Console.WriteLine($"{result}");
+        Console.WriteLine($"{(double)Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2))}");
+    }
+}
