a8482b50a45567019bdc50a18931fed88d56c79b
Malikov Vladimir <v.a.jack@rambler.ru>
2022-09-07 09:32:56 +0300

    Task19

--- Sem3/HomeWork/HW_Sem3/Task19/Task19.cs
+++ Sem3/HomeWork/HW_Sem3/Task19/Task19.cs
@@ -1,3 +1,9 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите 5-изначное число: ");
+var number = Console.ReadLine()!.ToString();
+
+if (number.Reverse().SequenceEqual(number))
+    Console.WriteLine("Ваше число - палиндром");
+else
+    Console.WriteLine("Ваше число - НЕ палиндром");
