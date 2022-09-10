a6b95bfb81d4f8738e3367a73dd9cf8ec64d58ca
Malikov Vladimir <v.a.jack@rambler.ru>
2022-09-10 17:43:46 +0300

    Task29

--- Sem4/HW_Sem4/Task29/Task29.cs
+++ Sem4/HW_Sem4/Task29/Task29.cs
@@ -1,3 +1,16 @@
 ﻿// Пользователь вводит 8 чисел. Поместите их в массив и распечатайте.
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ведите 8 чисел");
+
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = int.Parse(Console.ReadLine()!);
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (i == 0) Console.Write($"[{array[i]}, ");
+    if (i > 0 && i < array.Length - 1) Console.Write($"{array[i]}, ");
+    if (i == array.Length - 1) Console.Write($"{array[i]}]");
+}
