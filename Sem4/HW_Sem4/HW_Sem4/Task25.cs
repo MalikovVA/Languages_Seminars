b15e12369646d6747f17e2a5a768e4d3e93a944a
Malikov Vladimir <v.a.jack@rambler.ru>
2022-09-10 10:51:22 +0300

    Sem4

---
+++ Sem4/HW_Sem4/HW_Sem4/Task25.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+Console.WriteLine("Input first number: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Input second number: ");
+int B = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= B; i++)
+    var degree = A * A;
+
+
+
+Console.WriteLine($"{degree}");
