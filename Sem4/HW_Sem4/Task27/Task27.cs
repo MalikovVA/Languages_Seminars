8ce85d191e23551123fbd6a47672532d6908192f
Malikov Vladimir <v.a.jack@rambler.ru>
2022-09-10 12:49:20 +0300

    Task27

--- Sem4/HW_Sem4/Task27/Task27.cs
+++ Sem4/HW_Sem4/Task27/Task27.cs
@@ -1,3 +1,13 @@
 ﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-Console.WriteLine("Hello, World!");
+Console.Write("Input a number: ");
+int num = int.Parse(Console.ReadLine());
+
+int sum = 0;
+int n = num;
+while(n > 0)
+{
+    sum = sum + n % 10;
+    n = n / 10;
+}
+Console.WriteLine($"Сумма цифр в числе {num} - {sum}");
