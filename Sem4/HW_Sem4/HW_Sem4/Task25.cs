0c5c5e08b9768cf01bfdd13844e0f635559c6d54
Malikov Vladimir <v.a.jack@rambler.ru>
2022-09-10 11:48:29 +0300

    Task25

--- Sem4/HW_Sem4/HW_Sem4/Task25.cs
+++ Sem4/HW_Sem4/HW_Sem4/Task25.cs
@@ -1,14 +1,22 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
 
-Console.WriteLine("Input first number: ");
+Console.Write("Input first number: ");
 int A = int.Parse(Console.ReadLine());
-Console.WriteLine("Input second number: ");
+Console.Write("Input second number: ");
 int B = int.Parse(Console.ReadLine());
+double degree = 1;
 
-for (int i = 1; i <= B; i++)
-    var degree = A * A;
-
-
-
-Console.WriteLine($"{degree}");
+if (B == 0) Console.WriteLine($"Результатом возведения числа {A} в степень числа {B} является {degree}");
+else if (B < 0) Console.WriteLine("Отрицательная степень числа не допустима");
+else
+{
+    for (int i = 1; i < B; i++)
+    {
+        degree = degree * A;
+        Console.Write($"{degree}, ");
+    }
+    degree = degree * A;
+    Console.WriteLine($"{degree}. ");
+    Console.WriteLine($"Результатом возведения числа {A} в степень числа {B} является {degree}");
+}
