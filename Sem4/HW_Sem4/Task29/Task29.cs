0352608218e0dac94f3abb165d8fab777f5b289b
Malikov Vladimir <v.a.jack@rambler.ru>
2022-09-10 18:18:38 +0300

    Task29Fix

--- Sem4/HW_Sem4/Task29/Task29.cs
+++ Sem4/HW_Sem4/Task29/Task29.cs
@@ -1,5 +1,6 @@
 ﻿// Пользователь вводит 8 чисел. Поместите их в массив и распечатайте.
 
+// Решение через 8 чисел введенных отельно
 Console.WriteLine("Ведите 8 чисел");
 
 int[] array = new int[8];
@@ -8,6 +9,19 @@ for (int i = 0; i < array.Length; i++)
 {
     array[i] = int.Parse(Console.ReadLine()!);
 }
+for (int i = 0; i < array.Length; i++)
+{
+    if (i == 0) Console.Write($"[{array[i]}, ");
+    if (i > 0 && i < array.Length - 1) Console.Write($"{array[i]}, ");
+    if (i == array.Length - 1) Console.Write($"{array[i]}]");
+}
+
+// Решение через строку
+
+Console.WriteLine("Ведите числа массива через пробел или запятую ");
+string arr = Console.ReadLine();
+string[] array = arr.Split(' ', ',');
+
 for (int i = 0; i < array.Length; i++)
 {
     if (i == 0) Console.Write($"[{array[i]}, ");
