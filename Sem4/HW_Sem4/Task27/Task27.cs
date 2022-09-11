1bc3dd50986248a2bc14839cad2cb6cb13286f87
Malikov Vladimir <v.a.jack@rambler.ru>
2022-09-11 16:32:42 +0300

    CodeOptimization

--- Sem4/HW_Sem4/Task27/Task27.cs
+++ Sem4/HW_Sem4/Task27/Task27.cs
@@ -8,11 +8,11 @@ static void SumOfDigits()
 {
     int num = int.Parse(Console.ReadLine());
     int sum = 0;
-    int n = num;
-    while (n > 0)
+    int temp = num;
+    while (temp > 0)
     {
-        sum = sum + n % 10;
-        n = n / 10;
+        sum += temp % 10;
+        temp = temp / 10;
     }
     Console.WriteLine($"Сумма цифр в числе {num} - {sum}");
 }
--- Sem4/HW_Sem4/Task29/Task29.cs
+++ Sem4/HW_Sem4/Task29/Task29.cs
@@ -14,8 +14,8 @@ static void PrintArray<T>(T[] array)
     for (int i = 0; i < array.Length; i++)
     {
         if (i == 0) Console.Write($"[{array[i]}, ");
-        if (i > 0 && i < array.Length - 1) Console.Write($"{array[i]}, ");
-        if (i == array.Length - 1) Console.Write($"{array[i]}]");
+        else if (i > 0 && i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else if (i == array.Length - 1) Console.Write($"{array[i]}]");
     }
 }
 
