377459e2a1d4b607fdd7a72c81a902483f6c6d16
Malikov Vladimir <v.a.jack@rambler.ru>
2022-09-16 21:49:53 +0300

    ClassWork

---
+++ Sem6/ClassWork/Task40/Sem6CW/Task40.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+int Input(string i)
+{
+    Console.WriteLine($"Enter {i} number:");
+    return int.Parse(Console.ReadLine()!);
+}
+
+int a = Input("first");
+int b = Input("second");
+int c = Input("third");
+
+if (a + b > c && a + c > b && b + c > a)
+{
+    Console.WriteLine("All good");
+}
+else Console.WriteLine("Not a triangle");
+
+//Console.WriteLine("Enter first number:");
+//int a = int.Parse(Console.ReadLine()!);
+
+//Console.WriteLine("Enter second number:");
+//int b = int.Parse(Console.ReadLine()!);
+
+//Console.WriteLine("Enter third number:");
+//int c = int.Parse(Console.ReadLine()!);
---
+++ Sem6/ClassWork/Task40/Task42/Task42.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+//45-> 101101
+//3-> 11
+//2-> 10
+
+Console.WriteLine("Input your number:");
+
+int input = int.Parse(Console.ReadLine()!);// get number 23
+// Для перевода в двоичную систему необходимо разделить на 2 и посмотреть на остаток от деления на 2
+string array = string.Empty;
+
+while (input > 0)
+{
+    int digit = input % 2;
+    input = input / 2;
+    array = digit + array;
+}
+
+Console.WriteLine(array);
