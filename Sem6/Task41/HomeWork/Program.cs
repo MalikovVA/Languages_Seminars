c2bfdcb626088cb5270bef219392a2a5212044df
Malikov Vladimir <v.a.jack@rambler.ru>
2022-09-17 15:50:57 +0300

    Sem6HW

---
+++ Sem6/Task41/HomeWork/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Решение через последовательный ввод с запросом у пользователя количества элементов.
+
+int[] array = GetArrayLengthFromUser();
+
+CountInputPositiveNumbers(array);
+
+static int[] GetArrayLengthFromUser()
+{
+    Console.WriteLine("Сколько чисел проверим?");
+    int[] array = new int[int.Parse(Console.ReadLine()!)];
+    return array;
+}
+
+static void CountInputPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Ведите {i + 1} число:");
+        array[i] = int.Parse(Console.ReadLine()!);
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Из введенных Вами чисел больше нуля - {count}.");
+}
+
+// Решение через свободный ввод
+
+//Console.Write("Введите элементы через пробел: ");
+//int[] array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+//GetPositiveNumbers(array);
+
+//static void GetPositiveNumbers(int[] array)
+//{
+//    int count = 0;
+
+//    for (int i = 0; i < array.Length; i++)
+//    {
+//        if (array[i] > 0)
+//        {
+//            count++;
+//        }
+//    }
+
+//    Console.WriteLine($"Из введенных Вами чисел больше нуля - {count}.");
+//}
---
+++ Sem6/Task41/Task43/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Введите k1: ");
+double k1 = Double.Parse(Console.ReadLine()!);
+Console.Write("Введите b1: ");
+double b1 = Double.Parse(Console.ReadLine()!);
+Console.Write("Введите k2: ");
+double k2 = Double.Parse(Console.ReadLine()!);
+Console.Write("Введите b2: ");
+double b2 = Double.Parse(Console.ReadLine()!);
+
+if (k1 == k2)
+{
+    Console.WriteLine("Ваши прямые параллельные");
+}
+else if ((k1 / k2) == (b1 / b2))
+{
+    Console.WriteLine("Ваши прямые совпадают");
+}
+else
+{
+    double x = Math.Round(((b2 - b1) / (k1 - k2)), 3);
+    double y = Math.Round((k1 * x + b1), 3);
+    Console.WriteLine($"Пересечение в точке: ({x};{y})");
+}
