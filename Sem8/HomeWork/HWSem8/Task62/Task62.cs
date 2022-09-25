3e0c43c64eac758a308df0326d8ea5e7fd2fc996
Malikov Vladimir <v.a.jack@rambler.ru>
2022-09-25 19:29:07 +0300

    Task62

--- Sem8/HomeWork/HWSem8/Task62/Task62.cs
+++ Sem8/HomeWork/HWSem8/Task62/Task62.cs
@@ -1,4 +1,41 @@
 ﻿// Заполните спирально массив 4 на 4.
 
+int height = EnterInt("Enter height: ");
+int width = EnterInt("Enter width: ");
 
-Console.WriteLine("Hello, World!");
+int[,] array = new int[height, width];
+PrintSpiralArray(array);
+
+int EnterInt(string prompt)
+{
+    Console.Write(prompt);
+    return int.Parse(Console.ReadLine()!);
+}
+
+static void PrintSpiralArray(int[,] array)
+{
+    int row = 0, col = 0, dx = 1, dy = 0, dirChanges = 0, gran = array.GetLength(1);
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[row, col] = i + 1;
+        if (--gran == 0)
+        {
+            gran = array.GetLength(1) * (dirChanges % 2) + array.GetLength(0) * ((dirChanges + 1) % 2) - (dirChanges / 2 - 1) - 2;
+            int temp = dx;
+            dx = -dy;
+            dy = temp;
+            dirChanges++;
+        }
+        col += dx;
+        row += dy;
+    }
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine("\n");
+    }
+}
